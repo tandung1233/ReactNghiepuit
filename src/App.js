41a11f8a613870daa245e343b4a96155d6363015
TanDung <nguyentandung9111@gmail.com>
2022-11-16 10:49:03 +0700

    Bài 10: State

--- src/App.js
+++ src/App.js
@@ -1,54 +1,67 @@
 import "./App.css";
-import Product from "./Component/Product";
 import { Component } from "react";
 class App extends Component {
-  onClick() {
-    console.log("Đây là component");
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: [
+        {
+          id: 1,
+          name: "Iphone 6 Plus",
+          price: 16000000,
+          image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
+          status: true,
+        },
+        {
+          id: 2,
+          name: "Iphone 10 Plus",
+          price: 10000000,
+          image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
+          status: true,
+        },
+        {
+          id: 2,
+          name: "Iphone 3 Plus",
+          price: 13000000,
+          image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
+          status: false,
+        },
+        {
+          id: 3,
+          name: "Iphone 2 Plus",
+          price: 12000000,
+          image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
+          status: true,
+        },
+      ],
+      isActive: true,
+    };
+    //Cách 1: cách 2 cứ sài onSetState = () =>{} cho khỏe tại nó kiểu bắt khai báo mới được sử dụng
+    // this.onSetState = this.onSetState.bind(this);
   }
-  //Sử dụng ref gọi ra để sử dụng lấy giá trị nơi ô input á củng dễ
-  onAddProduct = () => {
-    console.log(this.refs.name.value);
+  onSetState = () => {
+    if (this.state.isActive === true) {
+      //Phải sử dụng setState chứ k sử dụng this.state.isActive được mà phải thông qua cái phương thức đó
+      this.setState({
+        isActive: false,
+      });
+    } else {
+      this.setState({
+        isActive: true,
+      });
+    }
   };
   render() {
-    var product = [
-      {
-        id: 1,
-        name: "Iphone 6 Plus",
-        price: 16000000,
-        image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
-        status: true,
-      },
-      {
-        id: 2,
-        name: "Iphone 10 Plus",
-        price: 10000000,
-        image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
-        status: true,
-      },
-      {
-        id: 2,
-        name: "Iphone 3 Plus",
-        price: 13000000,
-        image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
-        status: false,
-      },
-      {
-        id: 3,
-        name: "Iphone 2 Plus",
-        price: 12000000,
-        image: "https://www.bootdey.com/img/Content/avatar/avatar7.png",
-        status: false,
-      },
-    ];
-    let element = product.map((product, intdex) => {
+    let element = this.state.product.map((product, index) => {
       if (product.status === true)
         return (
-          <Product
-            key={product.id}
-            name={product.name}
-            price={product.price}
-            image={product.image}
-          ></Product>
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{product.name}</td>
+            <td>
+              <span className="label label-success">{product.price}</span>
+            </td>
+          </tr>
         );
       else {
         return "";
@@ -56,26 +69,23 @@ class App extends Component {
     });
     return (
       <div className="container">
-        <h2>Thêm sản phẩm</h2>
-        <label>Tên sản phẩm</label>
-        <input type="text" className="form-control" ref="name" />
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={this.onAddProduct}
-        >
-          Lưu
-        </button>
-        <div className="row">
-          {/* Truyền các dữ liệu cha xuống con rồi cái con gọi lại bằng thuộc tính props.name,.. */}
-          {element}
-        </div>
+        <h1 align="center">State</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">STT</th>
+              <th scope="col">Tên Sản Phẩm</th>
+              <th scope="col">Giá</th>
+            </tr>
+          </thead>
+          <tbody>{element}</tbody>
+        </table>
         <button
           type="button"
           className="btn btn-warning"
-          onClick={this.onClick}
+          onClick={this.onSetState}
         >
-          Click me
+          Active: {this.state.isActive === true ? "true" : "false"}
         </button>
       </div>
     );
